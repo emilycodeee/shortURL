9b10d0f6d2d8e64b8ade08de4530b9d404e44442
emily_chen <emilycodeee@gmail.com>
2021-02-21 00:00:52 +0800

    feat : install body-parser

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
+
 
 const app = express()
 const port = 3000
@@ -10,6 +12,9 @@ const ShortURL = require('./models/shorturl')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
 // require config profile
 require('./config/mongoose')
 
@@ -17,12 +22,22 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+// redirect 
 app.get('/:shorten', (req, res) => {
   const shortenName = req.params.shorten
   return ShortURL.findOne({ shorten: shortenName })
     .lean()
     .then(relink => res.redirect(relink.origurl))
     .catch(error => console.log(error))
+})
+
+//new 
+
+app.post('/', (req, res) => {
+  const origUrl = req.body.url
+  console.log('origUrl', origUrl)
+  // console.log(req.query)
+  // return res.render('index', { originalUrl })
 })
 
 app.listen(port, () => {
