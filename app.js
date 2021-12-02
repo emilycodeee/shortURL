ff9f1d9668fd1d992f226bcded3b4014918cb8a1
Emily Chen <emilycodeee@gmail.com>
2021-12-02 14:35:53 +0800

    Edit README.md

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const exphbs = require('express-handlebars')
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // setting template engine
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // require routes & config profile
-const routes = require('./routes')
-require('./config/mongoose')
+const routes = require("./routes");
+require("./config/mongoose");
 
 // routes setting - Read
-app.use(routes)
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`express is listening on http://localhost:${PORT}`)
-})
-
+  console.log(`express is listening on http://localhost:${PORT}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,65 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const ShortURL = require("../models/shorturl");
+const shortenGenerator = require("../shortenGenerator");
 
-const express = require('express')
-const router = express.Router()
-const ShortURL = require('../models/shorturl')
-const shortenGenerator = require('../shortenGenerator')
+//home page
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
-
-//home page 
-router.get('/', (req, res) => {
-  res.render('index')
-})
-
-// redirect 
-router.get('/:shorten', (req, res) => {
-  const shortenName = req.params.shorten
-  ShortURL.find({ shorten: shortenName })
+// redirect
+router.get("/:shorten", (req, res) => {
+  const shortenName = req.params.shorten;
+  ShortURL.findOne({ shorten: shortenName })
     .lean()
     .then((relink) => {
       if (relink) {
-        res.status(301).redirect(relink[0].originUrl)
+        console.log(relink);
+        res.status(301).redirect(relink.originUrl);
       }
     })
-    .catch(() => { res.sendStatus(404) })
-})
-
+    .catch(() => {
+      res.sendStatus(404);
+    });
+});
 
 // generate shorten url - variable
-const localhost = 'http://localhost:3000/'
-const herokulink = 'https://intense-beyond-20199.herokuapp.com/'
+const localhost = "http://localhost:3000/";
+const herokulink = "https://intense-beyond-20199.herokuapp.com/";
 
-const mainUrl = process.env.NODE_ENV ? herokulink : localhost
-let newShorten = ''
+const mainUrl = process.env.NODE_ENV ? herokulink : localhost;
+let newShorten = "";
 
 // generate shorten url - post
-router.post('/', (req, res) => {
-
-  const newUrl = req.body.url
-
+router.post("/", (req, res) => {
+  const newUrl = req.body.url;
   ShortURL.find()
     .lean()
     .then((urlList) => {
-      newShorten = urlList.find((eachUrl) => eachUrl.originUrl === newUrl)
+      newShorten = urlList.find((eachUrl) => eachUrl.originUrl === newUrl);
       // check existed originURL
       if (newShorten) {
-        newShorten = mainUrl + newShorten.shorten
-        return res.render('success', { newShorten, newUrl })
+        newShorten = mainUrl + newShorten.shorten;
+        return res.render("success", { newShorten, newUrl });
       }
 
-      let shorten = shortenGenerator()
-      newShorten = mainUrl + shorten
+      let shorten = shortenGenerator();
+      newShorten = mainUrl + shorten;
       // check existed shorten
       while (urlList.some((eachUrl) => eachUrl.shorten === shorten)) {
-        shorten = shortenGenerator()
+        shorten = shortenGenerator();
       }
       // create new data
       return ShortURL.create({
         originUrl: newUrl,
-        shorten: shorten
-      })
+        shorten: shorten,
+      });
     })
-    .then(() => res.render('success', { newShorten, newUrl }))
-    .catch(error => console.log(error))
-})
+    .then(() => res.render("success", { newShorten, newUrl }))
+    .catch((error) => console.log(error));
+});
 
-module.exports = router
+module.exports = router;
